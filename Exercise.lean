ad439f7d5e04765feac90e3959be1562720893ad
Ondanaoto <n.onda.go20@gmail.com>
2023-11-28 20:28:40 +0900

    lean3用に変更

--- Exercise.lean
+++ Exercise.lean
@@ -1,9 +1,9 @@
-import Mathlib.Init.Data.Nat.Basic
+open nat
 
-def sum_to (n : ℕ) : ℕ :=
+def sum_to (n : nat) : nat :=
   match n with
   | .zero => 0
   | .succ m => n + sum_to m
 
-theorem sum_to_eq (n : ℕ) : sum_to n = n * (n + 1) / 2 :=
+theorem sum_to_eq (n : nat) : sum_to n = n * (n + 1) / 2 :=
   sorry
--- Main.lean
+++
@@ -1,4 +0,0 @@
-import «Exercise»
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
